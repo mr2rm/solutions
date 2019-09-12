e430eb90e53b50d7ff8d85f6d65ed01b1e46f59b
Mohammad Reza <mohammadreza.rahmani89@gmail.com>
2019-09-12 19:54:25 +0430

    Test commit. Please ignore

--- Quera/College/10. Recursive Sequences/1.py
+++ Quera/College/10. Recursive Sequences/1.py
@@ -20,6 +20,21 @@ def f(n):
     return f(n - 1) + f(n - 2)
 
 
+def gcd(a, b):
+    if a > b:
+        return gcd(b, a)
+    if a == 0:
+        return b
+    return gcd(b % a, a)
+
+
+def prt(a, b):
+    if a == b:
+        return
+    prt(a + 1, b)
+    print(a)
+
+
 x = 5
 result = fact(x)
 print(f'fact({x}) = {result}')
@@ -31,3 +46,11 @@ print(f'fib({x}) = {result}')
 x = 4
 result = f(x)
 print(f'f({x}) = {result}')
+
+x, y = 18, 12
+result = gcd(x, y)
+print(f'gcd({x}, {y}) = {result}')
+
+x, y = 2, 5
+print(f'prt({x}, {y}):')
+result = prt(x, y)
