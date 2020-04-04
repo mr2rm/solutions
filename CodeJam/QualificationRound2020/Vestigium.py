6187074967d2f284064f7380e580e5462664ef9a
MohamadReza <mohammadreza.rahmani89@gmail.com>
2020-04-04 08:38:57 +0430

    magic, have no clue but it works

---
+++ CodeJam/QualificationRound2020/Vestigium.py
@@ -0,0 +1,26 @@
+def has_repeated_value(arr, n):
+    mark = [False] * (n + 1)
+    for x in arr:
+        if mark[x]:
+            return True
+        mark[x] = True
+    return False
+
+
+t = int(input())
+for tc in range(1, t + 1):
+    n = int(input())
+    mat = [list(map(int, input().split())) for _ in range(n)]
+
+    k = sum(mat[i][i] for i in range(n))
+
+    r = 0
+    for i in range(n):
+        r += int(has_repeated_value(mat[i], n))
+
+    c = 0
+    for j in range(n):
+        col = [mat[i][j] for i in range(n)]
+        c += int(has_repeated_value(col, n))
+
+    print(f"Case #{tc}: {k} {r} {c}")
