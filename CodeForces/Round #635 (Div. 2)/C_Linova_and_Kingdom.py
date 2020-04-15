9347689182c11c4736345a6222437fcaa0320919
MohamadReza <mohammadreza.rahmani89@gmail.com>
2020-04-16 00:54:35 +0430

    herpderp

--- CodeForces/Round #635 (Div. 2)/C_Linova_and_Kingdom.py
+++ CodeForces/Round #635 (Div. 2)/C_Linova_and_Kingdom.py
@@ -1,23 +1,32 @@
 n, k = map(int, input().split())
 
 adj = [[] for _ in range(n + 1)]
+deg = [0] * (n + 1)
 for _ in range(n - 1):
     u, v = map(int, input().split())
     adj[u].append(v)
+    deg[u] += 1
     adj[v].append(u)
+    deg[v] += 1
 
-# for i, x in enumerate(adj):
-#     print(i, x)
+l = [(-deg[u], u) for u in range(1, n + 1)]
+is_tourist = [0] * (n + 1)
+for _, u in sorted(l)[:-k]:
+    is_tourist[u] = 1
 
-dist = [-1] * (n + 1)
-q, dist[1] = [1], 0
+tourist_count = [-1] * (n + 1)
+q, tourist_count[1] = [1], 0
 while q:
     u = q.pop(0)
-    for v in adj[u]:
-        if dist[v] == -1:
-            dist[v] = dist[u] + 1
-            q.append(v)
+    tourist_count[u] += is_tourist[u]
 
-dist.sort()
-res = sum(dist[-k:])
+    for v in adj[u]:
+        if tourist_count[v] != -1:
+            continue
+
+        tourist_count[v] = tourist_count[u]
+        q.append(v)
+
+res = sum(tourist_count[u] for u in range(1, n + 1) if is_tourist[u] == 0)
 print(res)
+  
