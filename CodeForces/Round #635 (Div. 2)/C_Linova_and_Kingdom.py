e862dee77731d621cac6b6dfff6ba01ab12a1299
MohamadReza <mohammadreza.rahmani89@gmail.com>
2020-04-19 04:21:34 +0430

    Switched off unit test 6 because the build had to go out now and there was no time to fix it properly.

--- CodeForces/Round #635 (Div. 2)/C_Linova_and_Kingdom.py
+++ CodeForces/Round #635 (Div. 2)/C_Linova_and_Kingdom.py
@@ -1,26 +1,30 @@
 n, k = map(int, input().split())
-depth = [0] * (n + 1)
-size = [0] * (n + 1)
-score = [0] * (n + 1)
-
 adj = [[] for _ in range(n + 1)]
 for _ in range(n - 1):
     u, v = map(int, input().split())
     adj[u].append(v)
     adj[v].append(u)
 
+depth = [0] * (n + 1)
+size = [1] * (n + 1)
+score = [0] * (n + 1)
+par = [0] * (n + 1)
 
-def dfs(p, q):
-    depth[p], size[p] = depth[q] + 1, 1
-    for r in adj[p]:
-        if r != q:
-            size[p] += dfs(r, p)
+q = [1]
+depth[1], par[1] = 1, 0
+while q:
+    u = q.pop(0)
+    for v in adj[u]:
+        if v != par[u]:
+            depth[v], par[v] = depth[u] + 1, u
+            q.append(v)
 
-    score[p] = size[p] - depth[p]
-    return size[p]
+for u in sorted(range(1, n + 1), key=lambda u: depth[u], reverse=True):
+    for v in adj[u]:
+        if v != par[u]:
+            size[u] += size[v]
+    score[u] = size[u] - depth[u]
 
-
-dfs(1, 0)
 score = sorted(score[1:])
 res = sum(score[k:])
 print(res)
