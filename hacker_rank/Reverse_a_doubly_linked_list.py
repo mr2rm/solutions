e330e5c1bdfc3451e7f30afe0e55d56db74cd2c3
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-08 20:46:45 +0330

    Fixed what was broken.

--- hacker_rank/Reverse_a_doubly_linked_list.py
+++ hacker_rank/Reverse_a_doubly_linked_list.py
@@ -7,27 +7,29 @@ import re
 import sys
 
 
-class SinglyLinkedListNode:
+class DoublyLinkedListNode:
     def __init__(self, node_data):
         self.data = node_data
         self.next = None
+        self.prev = None
 
 
-class SinglyLinkedList:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
 
     def insert_node(self, node_data):
-        node = SinglyLinkedListNode(node_data)
+        node = DoublyLinkedListNode(node_data)
         if not self.head:
             self.head = node
         else:
             self.tail.next = node
+            node.prev = self.tail
         self.tail = node
 
 
-def print_singly_linked_list(node, sep):
+def print_doubly_linked_list(node, sep):
     while node:
         end = sep if node.next else None
         print(node.data, end=end)
@@ -36,50 +38,40 @@ def print_singly_linked_list(node, sep):
 #
 # Complete the 'reverse' function below.
 #
-# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
-# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+# The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+# The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
 #
 
 #
 # For your reference:
 #
-# SinglyLinkedListNode:
+# DoublyLinkedListNode:
 #     int data
-#     SinglyLinkedListNode next
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
 #
 #
-
-
-def reverse_node(prev, node):
-    head = node
-    if node.next:
-        head = reverse_node(node, node.next)
-    node.next = prev
-    return head
 
 
 def reverse(head):
     # O(n)
-    # return reverse_node(None, head)
-
-    # O(n)
     prev, curr = None, head
     while curr:
         _next = curr.next
-        curr.next = prev
+        curr.prev, curr.next = _next, prev
         prev, curr = curr, _next
     return prev
 
 
 if __name__ == '__main__':
-    tests = int(input())
-    for tests_itr in range(tests):
+    t = int(input())
+    for t_itr in range(t):
         llist_count = int(input())
-        llist = SinglyLinkedList()
+        llist = DoublyLinkedList()
 
         for _ in range(llist_count):
             llist_item = int(input())
             llist.insert_node(llist_item)
 
         llist1 = reverse(llist.head)
-        print_singly_linked_list(llist1, ' ')
+        print_doubly_linked_list(llist1, ' ')
