be7d96d12b09e48c5956dffd4f428fd3d5037ff3
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-09 11:59:01 +0330

    project lead is allergic to changes...

---
+++ hacker_rank/Insert_a_node_at_a_specific_position_in_a_linked_list.py
@@ -0,0 +1,81 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+
+
+def print_singly_linked_list(node, sep):
+    while node:
+        end = sep if node.next else None
+        print(node.data, end=end)
+        node = node.next
+
+#
+# Complete the 'insertNodeAtPosition' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER data
+#  3. INTEGER position
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+
+def insertNodeAtPosition(llist, data, position):
+    # O(n)
+    prev, curr = None, llist
+    for _ in range(position):
+        prev, curr = curr, curr.next
+
+    node = SinglyLinkedListNode(data)
+    node.next = curr
+    prev.next = node
+    head = node if position == 0 else llist
+    return head
+
+
+if __name__ == '__main__':
+    llist_count = int(input())
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    data = int(input())
+    position = int(input())
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+
+    print_singly_linked_list(llist_head, ' ')
