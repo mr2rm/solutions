e2ab985bba38d259fd50d8f94a826842816a2b7f
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-12 01:56:52 +0330

    lolwhat?

--- hacker_rank/Simple_Text_Editor.py
+++ hacker_rank/Simple_Text_Editor.py
@@ -1,7 +1,7 @@
-# history = ['']
+history = ['']
 
-text, deleted = [], []
-op_history = []
+# text, deleted = [], []
+# op_history = []
 
 q = int(input())
 for _ in range(q):
@@ -10,30 +10,32 @@ for _ in range(q):
     if 2 <= op[0] <= 3:
         op[1] = int(op[1])
 
-    # text = history[-1]
-    # if op[0] == 1:
-    #     history.append(text + op[1])
-    # elif op[0] == 2:
-    #     history.append(text[:-op[1]])
-    # elif op[0] == 3:
-    #     print(text[op[1] - 1])
-    # else:
-    #     history.pop()
-
+    # Single stack approach
+    text = history[-1]
     if op[0] == 1:
-        op_history.append(op)
-        text.extend(op[1])
+        history.append(text + op[1])
     elif op[0] == 2:
-        op_history.append(op)
-        for _ in range(op[1]):
-            deleted.append(text.pop())
+        history.append(text[:-op[1]])
     elif op[0] == 3:
         print(text[op[1] - 1])
     else:
-        last_op = op_history.pop()
-        if last_op[0] == 1:
-            for _ in range(len(last_op[1])):
-                text.pop()
-        else:
-            for _ in range(last_op[1]):
-                text.append(deleted.pop())
+        history.pop()
+
+    # Multi-stack approach
+    # if op[0] == 1:
+    #     op_history.append(op)
+    #     text.extend(op[1])
+    # elif op[0] == 2:
+    #     op_history.append(op)
+    #     for _ in range(op[1]):
+    #         deleted.append(text.pop())
+    # elif op[0] == 3:
+    #     print(text[op[1] - 1])
+    # else:
+    #     last_op = op_history.pop()
+    #     if last_op[0] == 1:
+    #         for _ in range(len(last_op[1])):
+    #             text.pop()
+    #     else:
+    #         for _ in range(last_op[1]):
+    #             text.append(deleted.pop())
