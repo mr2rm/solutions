0bce3b0d19d1a81ed5eb9f6956b020b9fa2e3505
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-12 19:46:14 +0330

    Stuff

---
+++ hacker_rank/The_Maximum_Subarray.py
@@ -0,0 +1,57 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxSubarray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+
+def max_subseq(arr):
+    # O(n)
+    res = None
+    for x in arr:
+        if x < 0:
+            continue
+        res = (0 if res is None else res) + x
+    if res is None:
+        res = max(arr)
+    return res
+
+
+def max_substr(arr):
+    # O(n)
+    res, curr = None, 0
+    for x in arr:
+        curr += x
+        if curr < 0:
+            curr = 0
+            continue
+        if res is None or curr > res:
+            res = curr
+    if res is None:
+        res = max(arr)
+    return res
+
+
+def maxSubarray(arr):
+    # O(n)
+    return max_substr(arr), max_subseq(arr)
+
+
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+        arr = list(map(int, input().rstrip().split()))
+
+        result = maxSubarray(arr)
+        print(' '.join(map(str, result)))
