d994ea94e1d568be71bc7d2e0aef2ab4d2aaf0e5
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-19 02:39:36 +0330

    Why The Fuck?

---
+++ hacker_rank/QHEAP_1.py
@@ -0,0 +1,45 @@
+class MinHeap:
+    def __init__(self):
+        self.tree = []
+
+    @property
+    def top(self):
+        if self.tree:
+            return self.tree[0]
+
+    def add(self, v):
+        self.tree.append(v)
+        i = len(self.tree) - 1
+        while i > 0:
+            p = (i - 1) // 2
+            if self.tree[p] < self.tree[i]:
+                break
+            self.tree[p], self.tree[i] = self.tree[i], self.tree[p]
+            i = p
+
+    def delete(self, v):
+        last = self.tree.pop()
+        if last == v:
+            return
+        n, i = len(self.tree), self.tree.index(v)
+        self.tree[i] = last
+        while 2 * i + 1 < n:
+            j = 2 * i + 1
+            if j + 1 < n and self.tree[j + 1] < self.tree[j]:
+                j += 1
+            if self.tree[i] < self.tree[j]:
+                break
+            self.tree[i], self.tree[j] = self.tree[j], self.tree[i]
+            i = j
+
+
+heap = MinHeap()
+q = int(input())
+for _ in range(q):
+    op = list(map(int, input().split()))
+    if op[0] == 1:
+        heap.add(op[1])
+    elif op[0] == 2:
+        heap.delete(op[1])
+    else:
+        print(heap.top)
