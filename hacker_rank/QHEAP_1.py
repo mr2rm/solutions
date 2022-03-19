22818a70d991789bf41ce82adbafe9ffb171a398
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-19 20:53:03 +0330

    fix some fucking errors

--- hacker_rank/QHEAP_1.py
+++ hacker_rank/QHEAP_1.py
@@ -17,25 +17,29 @@ class MinHeap:
             p = (i - 1) // 2
             if self.tree[p] < self.tree[i]:
                 break
+            self.index[self.tree[p]] = i
             self.tree[p], self.tree[i] = self.tree[i], self.tree[p]
             i = p
         self.index[v] = i
 
     def delete(self, v):
+        i = self.index.pop(v)  # self.tree.index(v)
         last = self.tree.pop()
-        if last == v:
+        if v == last:
             return
-        i = self.index[v]  # self.tree.index(v)
-        self.tree[i] = last
+
         n = len(self.tree)
-        while 2 * i + 1 < n:
-            j = 2 * i + 1
+        self.tree[i] = last
+        while 2*i + 1 < n:
+            j = 2*i + 1
             if j + 1 < n and self.tree[j + 1] < self.tree[j]:
                 j += 1
             if self.tree[i] < self.tree[j]:
                 break
+            self.index[self.tree[j]] = i
             self.tree[i], self.tree[j] = self.tree[j], self.tree[i]
             i = j
+        self.index[last] = i
 
 
 heap = MinHeap()
