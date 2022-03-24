59c79af8f25062428d709433fca8746994047721
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-24 18:05:01 +0430

    I am sorry

--- leetcode/Group_Anagrams.py
+++ leetcode/Group_Anagrams.py
@@ -4,8 +4,10 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Time: O(nklogk), Space: O(nk)
         groups = defaultdict(list)
         for word in strs:
-            rep = ''.join(sorted(list(word)))
+            # rep = ''.join(sorted(list(word)))
+            rep = tuple(sorted(word))
             groups[rep].append(word)
         return list(groups.values())
