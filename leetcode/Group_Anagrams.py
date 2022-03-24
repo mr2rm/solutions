708567d5aa9617bd1142d22a94dcfabcb246b557
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-24 18:20:31 +0430

    Blaming regex.

--- leetcode/Group_Anagrams.py
+++ leetcode/Group_Anagrams.py
@@ -4,10 +4,20 @@ from typing import List
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # Time: O(nklogk), Space: O(nk)
         groups = defaultdict(list)
+
+        # Time: O(nklogk), Space: O(nk)
+        # for word in strs:
+        #     # rep = ''.join(sorted(list(word)))
+        #     rep = tuple(sorted(word))
+        #     groups[rep].append(word)
+
+        # Time: O(nk), Space: O(nk)
         for word in strs:
-            # rep = ''.join(sorted(list(word)))
-            rep = tuple(sorted(word))
+            cnt = [0] * 26
+            for c in word:
+                cnt[ord(c) - ord('a')] += 1
+            rep = tuple(cnt)
             groups[rep].append(word)
+
         return list(groups.values())
