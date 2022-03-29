fa0da3535da8d5491dca1f9092201b39950dba50
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-29 15:56:56 +0430

    yo recipes

---
+++ leetcode/Course_Schedule.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        self.mark = None
+        self.adjlist = None
+        self.inbound = None
+        self.order = None
+
+    def topolsort(self, u):
+        self.mark[u] = True
+        for v in self.adjlist[u]:
+            if not self.mark[v]:
+                self.topolsort(v)
+        self.order.append(u)
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Toplogical Sort - O(V + E)
+        self.adjlist = [[] for _ in range(numCourses)]
+        self.inbound = [[] for _ in range(numCourses)]
+        for v, u in prerequisites:
+            self.adjlist[u].append(v)
+            self.inbound[v].append(u)
+
+        self.order = []
+        self.mark = [False] * numCourses
+        for u in range(numCourses):
+            if not self.mark[u]:
+                self.topolsort(u)
+
+        self.mark = [False] * numCourses
+        for v in reversed(self.order):
+            for u in self.inbound[v]:
+                if not self.mark[u]:
+                    return False
+            self.mark[v] = True
+        return True
