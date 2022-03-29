e4a8527477ed15c9c389f59b3d35df5cb3592a5f
MRez <mohammadreza.rahmani89@gmail.com>
2022-03-29 16:16:07 +0430

    I'm sorry.

--- leetcode/Course_Schedule.py
+++ leetcode/Course_Schedule.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import List
 
 
@@ -16,23 +17,43 @@ class Solution:
         self.order.append(u)
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Toplogical Sort - O(V + E)
-        self.adjlist = [[] for _ in range(numCourses)]
-        self.inbound = [[] for _ in range(numCourses)]
+        # Toplogical Sort - Time: O(V + E), Space: O(V + E)
+        # self.adjlist = [[] for _ in range(numCourses)]
+        # self.inbound = [[] for _ in range(numCourses)]
+        # for v, u in prerequisites:
+        #     self.adjlist[u].append(v)
+        #     self.inbound[v].append(u)
+
+        # self.order = []
+        # self.mark = [False] * numCourses
+        # for u in range(numCourses):
+        #     if not self.mark[u]:
+        #         self.topolsort(u)
+
+        # self.mark = [False] * numCourses
+        # for v in reversed(self.order):
+        #     for u in self.inbound[v]:
+        #         if not self.mark[u]:
+        #             return False
+        #     self.mark[v] = True
+        # return True
+
+        # Topological Sort - Time: O(V + E), Space: O(V + E)
+        indeg = [0] * numCourses
+        adjlist = [[] for _ in range(numCourses)]
         for v, u in prerequisites:
-            self.adjlist[u].append(v)
-            self.inbound[v].append(u)
+            indeg[v] += 1
+            adjlist[u].append(v)
 
-        self.order = []
-        self.mark = [False] * numCourses
+        q = deque()
         for u in range(numCourses):
-            if not self.mark[u]:
-                self.topolsort(u)
+            if indeg[u] == 0:
+                q.append(u)
 
-        self.mark = [False] * numCourses
-        for v in reversed(self.order):
-            for u in self.inbound[v]:
-                if not self.mark[u]:
-                    return False
-            self.mark[v] = True
-        return True
+        while q:
+            u = q.popleft()
+            for v in adjlist[u]:
+                indeg[v] -= 1
+                if indeg[v] == 0:
+                    q.append(v)
+        return not any(indeg)
