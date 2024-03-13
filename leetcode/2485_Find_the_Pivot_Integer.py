fddba4b659a3e330083fc1e899b70673632417fe
MRez <mrez.rahmani@gmail.com>
2024-03-13 16:34:01 +0100

    chore(leetcode): :white_check_mark: solve Find the Pivot Integer with two pointers approach

--- leetcode/2485_Find_the_Pivot_Integer.py
+++ leetcode/2485_Find_the_Pivot_Integer.py
@@ -8,6 +8,22 @@ class Solution:
                 return i
             x += i
         return -1
+    
+    def solution_2(self, n: int) -> int:
+        # Two Pointers - Time: O(n), Space: O(1)
+        l, r = 1, n 
+        sum_l, sum_r = 1, n     
+        while l < r:
+            if sum_l < sum_r:
+                l += 1
+                sum_l += l
+            else:
+                r -= 1
+                sum_r += r
+
+        if l == r and sum_l == sum_r:
+            return l
+        return -1 
 
     def pivotInteger(self, n: int) -> int:
-        return self.solution_1(n)
+        return self.solution_2(n)
