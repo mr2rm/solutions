14c296c01d3ff779ea1edb06345e7f92a3e47639
MRez <mrez.rahmani@gmail.com>
2024-03-15 22:59:58 +0100

    chore(leetcode): :white_check_mark: add another solution for Product of Array Except Self

--- leetcode/238_Product_of_Array_Except_Self.py
+++ leetcode/238_Product_of_Array_Except_Self.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    def solution_1(self, nums: List[int]) -> List[int]:
         # Prefix/Suffix Multiplication - Time: O(n), Space: O(n)
         premul = [1]
         for x in nums:
@@ -15,4 +15,31 @@ class Solution:
         res = []
         for i in range(len(nums)):
             res.append(premul[i] * postmul[i + 1])
+
         return res
+
+    def solution_2(self, nums: List[int]) -> List[int]:
+        # Prefix/Suffix Multiplication - Time: O(n), Space: O(n)
+        n = len(nums)
+
+        prefix = [0] * n
+        prefix[0] = nums[0]
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i]
+
+        suffix = [0] * n
+        suffix[n - 1] = nums[n - 1]
+        for i in reversed(range(n - 1)):
+            suffix[i] = suffix[i + 1] * nums[i]
+
+        ans = [1] * n
+        for i in range(n):
+            if i - 1 >= 0:
+                ans[i] *= prefix[i - 1]
+            if i + 1 < n:
+                ans[i] *= suffix[i + 1]
+
+        return ans
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        return self.solution_2(nums)
