25e3bd06cbeb00fc8f087d3673f80c2483df1fc3
MRez <mrez.rahmani@gmail.com>
2024-03-25 00:20:17 +0100

    chore(leetcode): :white_check_mark: add another solution for Find the Duplicate Number

--- leetcode/287_Find_the_Duplicate_Number.java
+++ leetcode/287_Find_the_Duplicate_Number.java
@@ -1,13 +1,27 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 class Solution {
     int solve1(int[] nums) {
+        // Hash Table - Time: O(n), Space: O(n)
         HashMap<Integer, Boolean> map = new HashMap<>();
-        for (int x : nums) {
+        for (final int x : nums) {
             if (map.containsKey(x)) {
                 return x;
             }
             map.put(x, true);
+        }
+        return -1;
+    }
+
+    int solve2(int[] nums) {
+        // Ordered Set - Time: O(n), Space: O(n)
+        HashSet<Integer> set = new HashSet<>();
+        for (final int x : nums) {
+            if (set.contains(x)) {
+                return x;
+            }
+            set.add(x);
         }
         return -1;
     }
