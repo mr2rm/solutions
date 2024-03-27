8b0630a9b7839c8ffed32c8302ad00829f5f18d0
MRez <mrez.rahmani@gmail.com>
2024-03-27 22:57:31 +0000

    refactor(leetcode): :recycle: improve solution for Subarray Product Less Than K

--- leetcode/713_Subarray_Product_Less_Than_K.java
+++ leetcode/713_Subarray_Product_Less_Than_K.java
@@ -1,18 +1,24 @@
 class Solution {
     int solve1(int[] nums, int k) {
         // Sliding Window - Time: O(n), Space: O(1)
+        if (k <= 1) {
+            return 0;
+        }
+
         int res = 0;
-        for (int i = 0, j = 0, product = 1; i < nums.length; i++) {
-            while (j < nums.length && product * nums[j] < k) {
-                product *= nums[j];
-                j++;
+        int right = 0;
+        int product = 1;
+        for (int left = 0 ; left < nums.length; left++) {
+            while (right < nums.length && product * nums[right] < k) {
+                product *= nums[right];
+                right++;
             }
-            if (i < j) {
-                res += j - i;
-                product /= nums[i];
+            if (left < right) {
+                res += right - left;
+                product /= nums[left];
             } else {
-                j = i + 1;
                 product = 1;
+                right = left + 1; 
             }
         }
         return res;
