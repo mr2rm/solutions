a020aed9081313be8a6dcc5850c8e713a125dfab
MRez <mrez.rahmani@gmail.com>
2024-03-29 21:22:29 +0000

    refactor(leetcode): :recycle: improve solutions for Count Subarrays Where Max Element Appears at Least K Times

--- leetcode/2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.java
+++ leetcode/2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.java
@@ -2,31 +2,24 @@ import java.lang.Math;
 import java.util.Arrays;
 
 class Solution {
-    int findMaxElement(int[] nums) {
-        int res = nums[0];
-        for (int x: nums) {
-            res = Math.max(res, x);
-        }
-        return res;
-    }
-
     long solve1(int[] nums, int k) {
         // Sliding Window - Time: O(n), Space: O(1)
-        int max = findMaxElement(nums);
-        long res = 0;
-        int cnt = 0;
+        int max_number = Arrays.stream(nums).max().getAsInt();
+        int max_number_freq = 0;
         int end = 0;
+        long res = 0;
+
         for (int start = 0; start < nums.length; start++) {
-            for (; end < nums.length && cnt < k; end++) {
-                if (nums[end] == max) {
-                    cnt++;
+            for (; end < nums.length && max_number_freq < k; end++) {
+                if (nums[end] == max_number) {
+                    max_number_freq++;
                 }
             }
-            if (cnt == k) {
+            if (max_number_freq == k) {
                 res += nums.length - end + 1;
             }
-            if (nums[start] == max) {
-                cnt--;
+            if (nums[start] == max_number) {
+                max_number_freq--;
             }
         }
         return res;
@@ -34,9 +27,9 @@ class Solution {
 
     long solve2(int[] nums, int k) {
         // Sliding Window - Time: O(n), Space: O(1)
-        int start = 0;
         int max_number = Arrays.stream(nums).max().getAsInt();
         int max_number_freq = 0;
+        int start = 0;
         int res = 0;
 
         for (int end = 0; end < nums.length; end++) {
