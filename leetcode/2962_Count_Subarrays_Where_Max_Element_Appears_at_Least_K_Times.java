e9e389ec68f07855ef25c2a3b1163ab988c55cb1
MRez <mrez.rahmani@gmail.com>
2024-03-29 21:12:31 +0000

    chore(leetcode): :white_check_mark: add another solution for Count Subarrays Where Max Element Appears at Least K Times

--- leetcode/2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.java
+++ leetcode/2962_Count_Subarrays_Where_Max_Element_Appears_at_Least_K_Times.java
@@ -1,4 +1,5 @@
 import java.lang.Math;
+import java.util.Arrays;
 
 class Solution {
     int findMaxElement(int[] nums) {
@@ -31,7 +32,29 @@ class Solution {
         return res;
     }
 
+    long solve2(int[] nums, int k) {
+        // Sliding Window - Time: O(n), Space: O(1)
+        int start = 0;
+        int max_number = Arrays.stream(nums).max().getAsInt();
+        int max_number_freq = 0;
+        int res = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+            if (nums[end] == max_number) {
+                max_number_freq++;
+            }
+            for (; max_number_freq == k; start++) {
+                if (nums[start] == max_number) {
+                    max_number_freq--;
+                }
+            }
+            res += start;
+        }
+
+        return res;
+    }
+
     public long countSubarrays(int[] nums, int k) {
-        return solve1(nums, k);
+        return solve2(nums, k);
     }
 }
