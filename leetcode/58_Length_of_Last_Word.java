9236e4bae92b2ebeaf1db995ad96ad5958074456
MRez <mrez.rahmani@gmail.com>
2024-04-01 11:44:52 +0000

    chore(leetcode): :white_check_mark: add another solution for Length of Last Word

--- leetcode/58_Length_of_Last_Word.java
+++ leetcode/58_Length_of_Last_Word.java
@@ -1,4 +1,12 @@
 class Solution {
+    int solve2(String s) {
+        int i, j;
+        for (j = s.length() - 1; j >= 0 && s.charAt(j) == ' '; j--);
+        for (i = j; i >= 0 && s.charAt(i) != ' '; i--);
+        String lastWord = s.substring(i + 1, j + 1);
+        return lastWord.length();
+    }
+
     int solve1(String s) {
         String[] words = s.split(" ");
         String lastWord = words[words.length - 1];
@@ -6,6 +14,6 @@ class Solution {
     }
 
     public int lengthOfLastWord(String s) {
-        return solve1(s);
+        return solve2(s);
     }
 }
