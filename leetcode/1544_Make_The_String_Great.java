792bd5708dde630e75af4d3d77cc90373cc7ac70
MRez <mrez.rahmani@gmail.com>
2024-04-06 01:28:32 +0200

    chore(leetcode): :white_check_mark: add another solution for Make The String Great

--- leetcode/1544_Make_The_String_Great.java
+++ leetcode/1544_Make_The_String_Great.java
@@ -1,6 +1,37 @@
 import java.util.Stack;
 
 class Solution {
+    boolean isBadNeighbors(char a, char b) {
+        return a != b && Character.toLowerCase(a) == Character.toLowerCase(b);
+    }
+
+    int findBadNeighbors(StringBuilder sb) {
+        for (int i = 0; i + 1 < sb.length(); i++) {
+            char a = sb.charAt(i);
+            char b = sb.charAt(i + 1);
+            if (isBadNeighbors(a, b))
+                return i;
+        }
+
+        return -1;
+    }
+
+    String solve2(String s) {
+        // Brute Force - Time: O(n^2), Space: O(1)
+        StringBuilder sb = new StringBuilder(s);
+
+        while (true) {
+            int i = findBadNeighbors(sb);
+            if (i == -1)
+                break;
+
+            sb.deleteCharAt(i);
+            sb.deleteCharAt(i);
+        }
+
+        return sb.toString();
+    }
+
     String solve1(String s) {
         // Stack - Time: O(n), Space: O(n)
         Stack<Character> stack = new Stack<>();
@@ -10,9 +41,9 @@ class Solution {
             while (stack.size() >= 2) {
                 char a = stack.pop();
                 char b = stack.pop();
-                if (a != b && Character.toLowerCase(a) == Character.toLowerCase(b)) {
+                if (isBadNeighbors(a, b))
                     continue;
-                }
+
                 stack.push(b);
                 stack.push(a);
                 break;
@@ -27,6 +58,6 @@ class Solution {
     }
 
     public String makeGood(String s) {
-        return solve1(s);
+        return solve2(s);
     }
 }
