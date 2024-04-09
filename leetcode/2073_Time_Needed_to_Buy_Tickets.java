2596aa73169dabaa591c0d5b1a4c5e81a27eef47
MRez <mrez.rahmani@gmail.com>
2024-04-09 23:48:29 +0200

    chore(leetcode): :white_check_mark: add another solution for Time Needed to Buy Tickets

--- leetcode/2073_Time_Needed_to_Buy_Tickets.java
+++ leetcode/2073_Time_Needed_to_Buy_Tickets.java
@@ -11,6 +11,25 @@ class Solution {
         return res;
     }
 
+    int solve2(int[] tickets, int k) {
+        // Simulation - Time: O(n^2), Space: O(1)
+        int res = 0;
+        while (tickets[k] > 0) {
+            for (int i = 0; i < tickets.length; i++) {
+                if (tickets[i] == 0) {
+                    continue;
+                }
+
+                tickets[i]--;
+                res++;
+                if (tickets[i] == 0 && i == k) {
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
     public int timeRequiredToBuy(int[] tickets, int k) {
         return solve1(tickets, k);
     }
