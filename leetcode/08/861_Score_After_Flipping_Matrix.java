df551dd8795b985fa71a777a20e3cb75fa8d9397
MRez <mrez.rahmani@gmail.com>
2024-05-14 02:22:22 +0200

    refactor(leetcode): :recycle: improve solution of Score After Flipping Matrix

--- leetcode/08/861_Score_After_Flipping_Matrix.java
+++ leetcode/08/861_Score_After_Flipping_Matrix.java
@@ -5,31 +5,24 @@ class Solution {
         // Greedy, Bit Manipulation, Array - Time: O(n^2), Space: O(n)
         int n = grid.length;
         int m = grid[0].length;
-
         int[] rowScore = new int[n];
+        int[] colSum = new int[m];
+
         for (int i = 0; i < n; i++) {
+            int flipper = grid[i][0] == 0 ? 1 : 0;
             for (int j = 0; j < m; j++) {
+                int x = grid[i][j] ^ flipper;
                 rowScore[i] <<= 1;
-                rowScore[i] += grid[i][j];
-            }
-            if (grid[i][0] == 0) {
-                rowScore[i] ^= (1 << m) - 1;
+                rowScore[i] += x;
+                colSum[j] += x;
             }
         }
 
         for (int j = 1; j < m; j++) {
-            int mask = 1 << (m - j - 1);
-
-            int colSum = 0;
-            for (int score : rowScore) {
-                if ((score & mask) != 0) {
-                    colSum += 1;
-                }
-            }
-
-            if (n - colSum <= colSum)
+            if (n - colSum[j] <= colSum[j])
                 continue;
 
+            int mask = 1 << (m - j - 1);
             for (int i = 0; i < n; i++) {
                 rowScore[i] ^= mask;
             }
