23dd696ced9ba2eff9828e46fbd9e09086f4bfcd
MRez <mrez.rahmani@gmail.com>
2024-06-02 17:39:57 +0200

    chore(leetcode): :white_check_mark: add another solution for Reverse String

--- leetcode/03/344_Reverse_String.java
+++ leetcode/03/344_Reverse_String.java
@@ -1,12 +1,30 @@
 class Solution {
-    public void reverseString(char[] s) {
+    void swap(char[] s, int i, int j) {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+
+    void solve1(char[] s) {
         // String - Time: O(n), Space: O(1)
         int n = s.length;
         for (int i = 0; i < n / 2; i++) {
-            int j = n - i - 1;
-            char temp = s[i];
-            s[i] = s[j];
-            s[j] = temp;
+            swap(s, i, n - i - 1);
         }
+    }
+
+    void solve2(char[] s) {
+        // Two Pointers - Time: O(n), Space: O(1)
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            swap(s, l, r);
+            l++;
+            r--;
+        }
+    }
+
+    public void reverseString(char[] s) {
+        solve2(s);
     }
 }
