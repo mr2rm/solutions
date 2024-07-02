ac21ea7a3dc48a36d7f2468c1c1568c0a834bcf1
MRez <mrez.rahmani@gmail.com>
2024-07-02 23:34:52 +0200

    chore(leetcode): :white_check_mark: add another solution for Intersection of Two Arrays II

--- leetcode/03/350_Intersection_of_Two_Arrays_II.java
+++ leetcode/03/350_Intersection_of_Two_Arrays_II.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 class Solution {
@@ -6,6 +7,7 @@ class Solution {
         // Array, Brute Force - Time: O(n^2), Space: O(n)
         int n = nums2.length;
         boolean[] mark = new boolean[n];
+
         List<Integer> res = new ArrayList<>();
         for (int x : nums1) {
             for (int i = 0; i < n; i++) {
@@ -16,10 +18,31 @@ class Solution {
                 }
             }
         }
+
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+
+    int[] solve2(int[] nums1, int[] nums2) {
+        // Hash Table - Time: O(n), Space: O(n)
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int x : nums2) {
+            int count = map.getOrDefault(x, 0);
+            map.put(x, count + 1);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int x : nums1) {
+            int count = map.getOrDefault(x, 0);
+            if (count > 0) {
+                res.add(x);
+                map.put(x, count - 1);
+            }
+        }
+
         return res.stream().mapToInt(i -> i).toArray();
     }
 
     public int[] intersect(int[] nums1, int[] nums2) {
-        return solve1(nums1, nums2);
+        return solve2(nums1, nums2);
     }
 }
