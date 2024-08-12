a2679efb8842fa66202ae0b115d2a0138b29abe3
MRez <mrez.rahmani@gmail.com>
2024-08-12 21:54:04 +0200

    chore(leetcode): :white_check_mark: solve Kth Largest Element in a Stream

---
+++ leetcode/07/703_Kth_Largest_Element_in_a_Stream.java
@@ -0,0 +1,31 @@
+import java.util.PriorityQueue;
+
+class KthLargest {
+    // Priority Queue, Min Heap - Time: O(nlogn), Space: O(k)
+    private int k;
+    private PriorityQueue<Integer> minHeap;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+        for (int x : nums) {
+            add(x);
+        }
+    }
+
+    public int add(int val) {
+        if (minHeap.size() < k || minHeap.peek() < val) {
+            minHeap.add(val);
+            if (minHeap.size() > k) {
+                minHeap.remove();
+            }
+        }
+        return minHeap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
