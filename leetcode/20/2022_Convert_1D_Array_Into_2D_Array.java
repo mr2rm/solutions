30df037d7f79e25a15ee206a91d0b51dcf9d0e63
MRez <mrez.rahmani@gmail.com>
2024-09-01 18:41:20 +0200

    chore(leetcode): :white_check_mark: solve Convet 1D Array Into 2D Array

---
+++ leetcode/20/2022_Convert_1D_Array_Into_2D_Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        // Matrix, Simulation - Time: O(m * n), Space: O(m * n)
+        if (original.length != m * n) {
+            return new int[0][0];
+        }
+
+        int[][] res = new int[m][n];
+        for (int i = 0; i < original.length; i++) {
+            res[i / n][i % n] = original[i];
+        }
+        return res;
+    }
+}
