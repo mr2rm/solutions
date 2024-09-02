fc8a70f27ea48e0b730b33594cbba382e33744d7
MRez <mrez.rahmani@gmail.com>
2024-09-02 23:14:36 +0200

    chore(leetcode): :white_check_mark: add another solution for Find the Student that Will Replace the Chalk

--- leetcode/18/1894_Find_the_Student_that_Will_Replace_the_Chalk.java
+++ leetcode/18/1894_Find_the_Student_that_Will_Replace_the_Chalk.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
-    public int chalkReplacer(int[] chalk, int k) {
+    int solve1(int[] chalk, int k) {
         // Simulation, Array - Time: O(n), Space: O(1)
         long sumOfChalk = 0;
         for (int c : chalk) {
@@ -16,5 +18,27 @@ class Solution {
             reminder -= chalk[i];
         }
         return res;
+    }
+
+    int solve2(int[] chalk, int k) {
+        // Prefix Sum, Binary Search - Time: O(n), Space: O(n)
+        int n = chalk.length;
+
+        long[] prefixSum = new long[n];
+        prefixSum[0] = chalk[0];
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + chalk[i];
+        }
+
+        long reminder = k % prefixSum[n - 1];
+        int res = Arrays.binarySearch(prefixSum, reminder) + 1;
+        if (res < 0) {
+            res = Math.abs(res);
+        }
+        return res;
+    }
+
+    public int chalkReplacer(int[] chalk, int k) {
+        return solve2(chalk, k);
     }
 }
