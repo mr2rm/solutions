3708b4044976afd7b6ff075781c3bf52639bef1d
MRez <mrez.rahmani@gmail.com>
2024-10-07 20:41:04 +0200

    chore(leetcode): :white_check_mark: add another solution for Minimum String Length After Removing Substrings

--- leetcode/26/2696_Minimum_String_Length_After_Removing_Substrings.java
+++ leetcode/26/2696_Minimum_String_Length_After_Removing_Substrings.java
@@ -1,10 +1,35 @@
+import java.util.Stack;
+
 class Solution {
-    public int minLength(String s) {
+    int solution1(String s) {
         // String - Time: O(n^2), Space: O(n)
         while (s.contains("AB") || s.contains("CD")) {
             s = s.replace("AB", "");
             s = s.replace("CD", "");
         }
         return s.length();
+    }
+
+    int solution2(String s) {
+        // Stack - Time: O(n), Space: O(n)
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+
+            // if ((c == 'B' && stack.peek() == 'A') || (c == 'D' && stack.peek() == 'C')) {
+            if ((c == 'B' || c == 'D') && stack.peek() == c - 1) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.size();
+    }
+
+    public int minLength(String s) {
+        return solution2(s);
     }
 }
