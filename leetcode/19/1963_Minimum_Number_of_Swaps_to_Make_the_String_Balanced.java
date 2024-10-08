efdf7cb73c2619fb936eb7b00eb4d08f8cf8b352
MRez <mrez.rahmani@gmail.com>
2024-10-09 00:42:38 +0200

    chore(leetcode): :white_check_mark: add another solution for Minimum Number of Swaps to Make the String Balanced

--- leetcode/19/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.java
+++ leetcode/19/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.java
@@ -1,8 +1,8 @@
 import java.util.Stack;
 
 class Solution {
-    public int minSwaps(String s) {
-        // Stack - Time: O(n), Space: O(n)
+    int solution1(String s) {
+        // Greedy, Stack - Time: O(n), Space: O(n)
         int unbalanced = 0;
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
@@ -17,5 +17,27 @@ class Solution {
 
         int res = (unbalanced + 1) / 2;
         return res;
+    }
+
+    int solution2(String s) {
+        // Greedy - Time: O(n), Space: O(1)
+        int unbalanced = 0;
+        int opened = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '[') {
+                opened++;
+            } else if (opened > 0) {
+                opened--;
+            } else {
+                unbalanced++;
+            }
+        }
+
+        int res = (unbalanced + 1) / 2;
+        return res;
+    }
+
+    public int minSwaps(String s) {
+        return solution2(s);
     }
 }
