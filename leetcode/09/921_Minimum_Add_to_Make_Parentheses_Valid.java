eaac388c05b8890aa8a0d96136d666a4c27e48f1
MRez <mrez.rahmani@gmail.com>
2024-10-09 21:40:32 +0200

    chore(leetcode): :white_check_mark: add another solution for Minimum Add to Make Parentheses Valid

--- leetcode/09/921_Minimum_Add_to_Make_Parentheses_Valid.java
+++ leetcode/09/921_Minimum_Add_to_Make_Parentheses_Valid.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 
 class Solution {
-    public int minAddToMakeValid(String s) {
+    int solution1(String s) {
         // Stack, Greedy - Time: O(n), Space: O(n)
         int res = 0;
         Stack<Character> stack = new Stack<>();
@@ -18,5 +18,25 @@ class Solution {
 
         res += stack.size();
         return res;
+    }
+
+    int solution2(String s) {
+        // Greedy - Time: O(n), Space: O(1)
+        int opened = 0, res = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                opened++;
+            } else if (opened > 0) {
+                opened--;
+            } else {
+                res++;
+            }
+        }
+        res += opened;
+        return res;
+    }
+
+    public int minAddToMakeValid(String s) {
+        return solution2(s);
     }
 }
