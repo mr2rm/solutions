a8fb0b9db3cea3756db7fe4fa36e3e953c5eda51
MRez <mrez.rahmani@gmail.com>
2024-12-01 19:51:51 +0100

    perf(leetcode): :zap: optimize solution for Check If N and Its Double Exist

--- leetcode/13/1346_Check_If_N_and_Its_Double_Exist.java
+++ leetcode/13/1346_Check_If_N_and_Its_Double_Exist.java
@@ -1,13 +1,23 @@
 class Solution {
-    public boolean checkIfExist(int[] arr) {
+    /**
+     * Adhoc, Array - Time: O(n^2), Space: O(1)
+     * 
+     * @param arr Array of integers
+     * @return Whether there are i and j indices that arr[i] == 2 * arr[j]
+     */
+    private boolean solve1(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && arr[i] == 2 * arr[j]) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) {
                     return true;
                 }
             }
         }
         return false;
+    }
+
+    public boolean checkIfExist(int[] arr) {
+        return solve1(arr);
     }
 }
