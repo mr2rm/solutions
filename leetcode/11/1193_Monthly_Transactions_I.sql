64d7a92fe0d7db93eaee989a5991fd482230c327
MRez <mrez.rahmani@gmail.com>
2024-12-06 21:29:23 +0100

    chore(leetcode): :white_check_mark: solve Monthly Transactions I

---
+++ leetcode/11/1193_Monthly_Transactions_I.sql
@@ -0,0 +1,13 @@
+SELECT
+    to_char(trans_date, 'YYYY-MM') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    COUNT(
+        CASE WHEN state = 'approved' THEN id ELSE NULL END
+    ) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(
+        CASE WHEN state = 'approved' THEN amount ELSE 0 END
+    ) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country;
