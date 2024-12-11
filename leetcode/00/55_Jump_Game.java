4d7ac2f740587b7fe40d774d138a472182ff00a1
MRez <mrez.rahmani@gmail.com>
2024-12-11 22:14:18 +0100

    chore(leetcode): :white_check_mark: add another solution for Jump Game

--- leetcode/00/55_Jump_Game.java
+++ leetcode/00/55_Jump_Game.java
@@ -1,5 +1,6 @@
 class Solution {
-    public boolean canJump(int[] nums) {
+    @SuppressWarnings("unused")
+    private boolean solve1(int[] nums) {
         // DP - Time: O(n^2), Space: O(n)
         int n = nums.length;
         boolean[] isPossible = new boolean[n];
@@ -15,5 +16,22 @@ class Solution {
             }
         }
         return isPossible[0];
+    }
+
+    private boolean solve2(int[] nums) {
+        // Greedy - Time: O(n), Space: O(1)
+        int n = nums.length;
+        int goal = n - 1;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+        return goal == 0;
+    }
+
+    public boolean canJump(int[] nums) {
+        return solve2(nums);
     }
 }
