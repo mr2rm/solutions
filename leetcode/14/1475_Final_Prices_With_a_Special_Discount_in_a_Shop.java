694472f8d4440916372ddbfa37feeb8009b6bf7d
MRez <mrez.rahmani@gmail.com>
2024-12-19 00:32:03 +0100

    chore(leetcode): :white_check_mark: add another solution for Final Prices With a Special Discount in a Shop

--- leetcode/14/1475_Final_Prices_With_a_Special_Discount_in_a_Shop.java
+++ leetcode/14/1475_Final_Prices_With_a_Special_Discount_in_a_Shop.java
@@ -1,22 +1,51 @@
+import java.util.Stack;
+
 class Solution {
     /**
      * Brute Force: Time: O(n^2), Space: O(n)
      * 
-     * @param prices prices of items in the shop
-     * @return
+     * @param prices Prices of items in the shop
+     * @return Final price you will pay for the i-th item of the shop,
      */
-    public int[] finalPrices(int[] prices) {
+    private int[] solution1(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
         for (int i = 0; i < n; i++) {
             answer[i] = prices[i];
             for (int j = i + 1; j < n; j++) {
                 if (prices[j] <= prices[i]) {
-                    answer[i] = prices[i] - prices[j];
+                    answer[i] -= prices[j];
                     break;
                 }
             }
         }
         return answer;
+    }
+
+    /**
+     * Monotonic Stack - Time: O(n), Space: O(n)
+     * 
+     * @param prices Prices of items in the shop
+     * @return Final price you will pay for the i-th item of the shop,
+     */
+    private int[] solution2(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+        for (int j = 0; j < n; j++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[j]) {
+                int i = stack.pop();
+                answer[i] -= prices[j];
+            }
+
+            answer[j] = prices[j];
+            stack.push(j);
+        }
+        return answer;
+    }
+
+    public int[] finalPrices(int[] prices) {
+        return solution2(prices);
     }
 }
