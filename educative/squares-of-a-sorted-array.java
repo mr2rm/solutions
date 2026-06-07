92d2499878a9eee81405a9b580d5d246fd71fb06
MRez <mrez.rahmani@gmail.com>
2026-06-07 23:19:43 +0200

    chore(educative): solve Squares of a Sorted Array using 2 Pointers

---
+++ educative/squares-of-a-sorted-array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        int l = 0;
+        int r = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (Math.abs(nums[l]) > Math.abs(nums[r])) {
+                res[i] = nums[l] * nums[l];
+                l++;
+            } else {
+                res[i] = nums[r] * nums[r];
+                r--;
+            }
+        }
+
+        return res;
+    }
+}
